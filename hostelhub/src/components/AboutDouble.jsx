636bd0e02f11b19770a223191d38ac9c42192d09
AyushmanSoni <ayushmanlsoni3660@gmail.com>
2024-03-04 13:28:56 +0530

    heading added in aboutdouble

--- hostelhub/src/components/AboutDouble.jsx
+++ hostelhub/src/components/AboutDouble.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const AboutDouble = () => {
   return (
-    <div>AboutDouble</div>
+    <div className='flex flex-row mx-36 my-20 mb-10 sm:flex-col sm:m-12 sm:mt-24 gap-x-32 lg:gap-x-8 text-[#575C75] bg-white'>
+        <div className='flex flex-col gap-y-10 max-w-[45vw] sm:max-w-[90vw]'>
+            <div className='flex flex-col gap-y-1 sm:text-center'>
+                <div className='font-semibold text-3xl'>
+                    Want to book your hostel room at the earliest ?
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
